237e72b8768b1b58b516ea2288e75cdccaf13062
Error203 <53088720+Error203@users.noreply.github.com>
2021-10-11 14:42:29 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -11,10 +11,17 @@ class DiscordClient:
 
 		self.log = qlogger.Logger("bot logs", level=level).get_logger("bot")
 		self.dev_id = 407598463010734080
+		self.my_id = 577946641600872487
 
 
 	def start_bot(self, token):
 		client = discord.Client()
+		command_prefix = "$$"
+		allowed_commands = ["dada", "help", "info"]
+		prefixed_allowed_commands = [command_prefix]
+
+		for cmd in allowed_commands:
+			prefixed_allowed_commands.append(command_prefix + cmd)
 
 		@client.event
 		async def on_ready():
@@ -32,28 +39,22 @@ class DiscordClient:
 
 		@client.event
 		async def on_message(message):
-			command_prefix = "$$"
-			command = message.content
-			allowed_commands = ["dada", "help", "info"]
-			prefixed_allowed_commands = [command_prefix]
-
-			for cmd in allowed_commands:
-				prefixed_allowed_commands.append(command_prefix + cmd)
-
 			handled_message = message.content.split(" ")
-			used_cmd = handled_message[0]
+			used_cmd = handled_message[0].lower()
 			command_arguments = handled_message[1:len(handled_message)]
+			user_main_nickname = message.author.name
 
-			if command.lower().startswith(command_prefix):
-				self.log.debug(f"{message.author.id} said# {message.content}")
+			if used_cmd.startswith(command_prefix):
+				self.log.info(f"{message.author.id} said# {message.content}")
 			
 				if message.author != client.user:
 					await message.delete()
 				
-				if command.lower() == command_prefix + "dada":
+				if used_cmd == command_prefix + "dada":
 					await message.channel.send("Еге-ж...")
 
-				if command.lower() == command_prefix + "help" or command.lower() == command_prefix + "info":
+				if used_cmd == command_prefix + "help" or used_cmd == command_prefix + "info":
+
 					mention_dev_name = f"<@!{self.dev_id}>"
 					user_id = message.author.id
 					mention_user_name = f"<@!{user_id}>"
@@ -62,20 +63,63 @@ class DiscordClient:
 					if user_id == self.dev_id:
 						status = "розробник"
 
-					if user_id == self.guild_owner_id:
+					elif user_id == self.guild_owner_id:
 						status = "засновник серверу"
 
-					await message.channel.send(f"За допомогою звертайтеся до {mention_dev_name} або відправивши **$$**(два символа долару).\n" + \
-						f"Ваш псевдонім на сервері: {mention_user_name}\nВаш ID: {user_id}\nВаш статус: {status}.")
+					elif user_id == self.my_id:
+						status = "Пан Робот"
 
-				if command.lower() == command_prefix + "clear":
+					if not command_arguments:
+						await message.channel.send(f"За допомогою звертайтеся до {mention_dev_name} або відправивши **$$**(два символа долару)\n" + \
+							f"Ваш псевдонім на сервері: {mention_user_name}\nВаш ID: {user_id}\nВаш статус: {status}")
+
+					if command_arguments:
+						try:
+							if command_arguments[0].isdigit():
+								requested_user_id = int(command_arguments[0])
+								self.log.debug(requested_user_id)
+								status = "тестер"
+
+								if requested_user_id == self.dev_id:
+									status = "розробник"
+
+								elif requested_user_id == self.guild_owner_id:
+									status = "засновник серверу"
+
+								elif requested_user_id == self.my_id:
+									status = "Пан Робот"
+
+							elif not command_arguments[0].isdigit():
+								requested_user_id = int(command_arguments[0][3:-1])
+								status = "тестер"
+
+								if requested_user_id == self.dev_id:
+									status = "розробник"
+
+								elif requested_user_id == self.guild_owner_id:
+									status = "засновник серверу"
+
+								elif requested_user_id == self.my_id:
+									status = "Пан Робот"
+
+								self.log.debug(requested_user_id)
+
+							mention_requested_user = f"<@!{requested_user_id}>"
+							await message.channel.send(f"Інформація про {mention_requested_user}:\nСтатус: {status}\nID: {requested_user_id}")
+
+						except Exception as e:
+							self.log.warning("not a serious error, but check it")
+							self.log.exception(e)
+
+
+				if used_cmd == command_prefix + "clear":
 					pass
 
-				if command.lower() == command_prefix:
+				if used_cmd == command_prefix:
 					await message.channel.send("Дозволені команди:\n\n{0}".format(f"\n".join(prefixed_allowed_commands)))
 
 				# message.content.lower() not in allowed_commands:
-				if command.lower() not in prefixed_allowed_commands:
+				if used_cmd not in prefixed_allowed_commands:
 					await message.channel.send(f"Вибач, але команда **{used_cmd}** не дозволена, або її не існує.\nІНФОРМАЦІЯ ДЛЯ РОЗРОБНИКА\n{command_arguments}")
 
 		client.run(token)
